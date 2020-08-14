c7339e13881550398cded64ac84af91fde781969
Shubham Jain <shubhamjain2908@gmail.com>
2020-08-15 02:52:21 +0530

    get comments route

--- comments/src/routes/get-comments.ts
+++ comments/src/routes/get-comments.ts
@@ -1,10 +1,25 @@
 import express, { Request, Response } from 'express';
+import { Comments, CommentsDoc } from '../models/comments';
 
 const router = express.Router();
 
+interface CommentResponse {
+    text: string,
+    username: string,
+    usersAge: number
+}
+
 router.get('/comments', async (req: Request, res: Response) => {
-    const storyId = req.params.storyId;
-    res.status(200).send({ storyId });
+    const storyId = +req.params.storyId;
+    const comments: Array<CommentsDoc> = await Comments.find({ storyId: storyId }).sort({ child_comment_count: -1 }).populate('User');
+    const commentResponse: Array<CommentResponse> = comments.map(comment => {
+        return {
+            text: comment.text,
+            username: comment.user.user,
+            usersAge: new Date(Date.now()).getFullYear() - new Date(comment.user.createdAt).getFullYear()
+        }
+    });
+    res.status(200).send({ comments: commentResponse });
 });
 
 export { router as getCommentsRouter };
